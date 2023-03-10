92a778e9964feb86c4b0269618ff3b328fb64c8f
jdabassett <jacobbassett@gmail.com>
2023-03-09 18:28:48 -0700

    small bug fix with state array

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   const date = new Date()
 
 
-  const [weekArray,setWeekArray] = React.useState([{day:"M",progress:100},{day:"T",progress:80},{day:"W",progress:60},{day:"T",progress:40},{day:"F",progress:30},{day:"S",progress:20},{day:"S",progress:10}])
+  const [weekArray] = React.useState([{day:"M",progress:100},{day:"T",progress:80},{day:"W",progress:60},{day:"T",progress:40},{day:"F",progress:30},{day:"S",progress:20},{day:"S",progress:10}])
 
   const weekElements = weekArray.map((day,index) => (
           <Weekday key={index} day={day.day} progress={day.progress} index={index+1} today={date.getDay()}/>
